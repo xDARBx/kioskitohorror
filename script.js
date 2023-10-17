66bef464456401780968298ee2c3bc73e42b9407
Daniel Alberto Rocha Bartaburu <89049112+xDARBx@users.noreply.github.com>
2023-10-17 19:21:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+
+var boton = document.getElementById("boton");
+boton.onclick = function(e) {
+e.preventDefault();
+var fboton = document.createElement("script");
+fboton.src = "script.js";
+document.querySelector("body").appendChild(fboton);
+}
+
+var productos = ["manzana","pera","banana","kiwi"]
+var precios = [300,450,400,900]
+var indice = prompt("elija un producto, ejemplo para manzanas tipear 0 así hasta 3," +" "+ productos + ":")
+var cantidad = prompt("elija la cantidad a llevar:")
+var precio = cantidad * precios[indice]
+
+alert("usted debe abonar: $" + precio + " por el producto deseado, " + productos[indice]+"," + " por la cantidad de: "+ cantidad + " unidades.")
